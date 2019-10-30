38a938c57d4d3385d8106d18eb5509a0c3f78baf
Milo Cress <cress@mit.edu>
2019-10-29 22:00:51 -0400

    Generalized Transform typeclass, TODO: make Transform a type

--- src/Transform.hs
+++ src/Transform.hs
@@ -3,7 +3,7 @@
 module Transform ( Transform, transform, inverse, compose
                  ) where
 
-class Transform p a where
+class Transform p where
   transform :: p a -> p a
   inverse   :: p a -> p a
   compose   :: (p a -> p a) -> (p a -> p a) -> p a -> p a
